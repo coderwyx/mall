067ef0c7aafe8b1b127b50a0f0d6f84de0ce6f6d
coderwyx <2514138002@qq.com>
2021-06-05 05:26:06 +0800

    初始化项目

---
+++ src/views/profile/profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> 
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "profile",
+  data () {
+   return {}
+  },
+   methods:{}
+}
+</script>
+
+<style>
+
+</style>
