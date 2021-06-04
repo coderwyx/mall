3897db16e886bc9ab162de2b06e09d003a0cfb2e
coderwyx <2514138002@qq.com>
2021-06-05 05:11:58 +0800

    初始化项目

---
+++ src/views/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> 
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data () {
+   return {}
+  },
+   methods:{}
+}
+</script>
+
+<style>
+
+</style>
