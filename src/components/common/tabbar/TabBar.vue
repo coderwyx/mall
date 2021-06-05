392803f74bb4f5296fba83aa027e923474358426
coderwyx <2514138002@qq.com>
2021-06-06 03:49:21 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  data () {
+   return {}
+  },
+   methods:{}
+}
+</script>
+
+<style>
+ 
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 3px rgba(100, 100, 100, 0.15);
+}
+
+
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div :style="activeStyle"><slot name="item-icon" ></slot></div>
+    <div :style="activeStyle"><slot name="item-text" ></slot></div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor:{
+        type:String,
+        default:"red"
+      }
+    },
+    data() {
+      return {
+        
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle(){
+        return this.isActive ? {color:this.activeColor} : {}
+      }
+      
+    },
+    methods: {
+      itemClick() {
+        this.$router.push(this.path)
+      }
+    }
+  }
+</script>
+
+<style>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+
+  }
+
+  .tab-bar-item span {
+    display: block;
+    font-size: 24px;
+    margin-top: 3px;
+  }
+
+  .tab-bar-item div {
+    font-size: 13px;
+  }
+ 
+</style>
